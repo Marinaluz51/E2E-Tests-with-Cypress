193b3a0d7c44f51a3a6309033f16090c403e31d0
Marina Luz <marina.luz@hotmail.com>
2024-03-26 22:42:58 +0100

    Update Heart.cy.jsx

--- src/components/Heart/Heart.cy.jsx
+++ src/components/Heart/Heart.cy.jsx
@@ -6,12 +6,11 @@ describe('Heart component', () => {
         cy.mount(<Heart />);
         cy.get("[data-testid='heart']").should('be.visible');
     });
+    
     it('should render correctly when selected', () => {
         cy.mount(<Heart selected />);
         cy.get("[data-testid='heart']").should('be.visible');
         cy.get("[data-testid='heart']").should('have.class', 'heart-selected');
     });
-
-
 
 })
